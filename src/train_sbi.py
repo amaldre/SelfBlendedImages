d361afd08414bd0837cb25cdcb5db05b9c26971a
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2025-06-26 11:02:28 +0200

    add wandb log auc

--- src/train_sbi.py
+++ src/train_sbi.py
@@ -106,95 +106,114 @@ def main(args):
 
     for epoch in range(n_epoch):
         np.random.seed(seed + epoch)
-        train_loss=0.
-        train_acc=0.
+        train_loss = 0.
+        train_acc = 0.
+        train_probs = []
+        train_targets = []
         model.train(mode=True)
-        for step,data in enumerate(tqdm(train_loader)):
-            img=data['img'].to(device, non_blocking=True).float()
-            target=data['label'].to(device, non_blocking=True).long()
-            output=model.training_step(img, target)
-            loss=criterion(output,target)
-            loss_value=loss.item()
+
+        for step, data in enumerate(tqdm(train_loader)):
+            img = data['img'].to(device, non_blocking=True).float()
+            target = data['label'].to(device, non_blocking=True).long()
+
+            output = model.training_step(img, target)
+            loss = criterion(output, target)
+            loss_value = loss.item()
             iter_loss.append(loss_value)
-            train_loss+=loss_value
-            acc=compute_accuray(F.log_softmax(output,dim=1),target)
-            train_acc+=acc
+            train_loss += loss_value
+
+            probs = F.softmax(output, dim=1)
+            acc = compute_accuray(torch.log(probs), target)
+            train_acc += acc
+
+            train_probs.append(probs[:, 1].detach().cpu())  # Assuming binary classification
+            train_targets.append(target.detach().cpu())
+
         lr_scheduler.step()
-        train_losses.append(train_loss/len(train_loader))
-        train_accs.append(train_acc/len(train_loader))
+        train_losses.append(train_loss / len(train_loader))
+        train_accs.append(train_acc / len(train_loader))
 
-        log_text="Epoch {}/{} | train loss: {:.4f}, train acc: {:.4f}, ".format(
-                        epoch+1,
-                        n_epoch,
-                        train_loss/len(train_loader),
-                        train_acc/len(train_loader),
-                        )
+        train_probs = torch.cat(train_probs).numpy()
+        train_targets = torch.cat(train_targets).numpy()
+        train_auc = roc_auc_score(train_targets, train_probs)
 
+        log_text = "Epoch {}/{} | train loss: {:.4f}, train acc: {:.4f}, train auc: {:.4f}, ".format(
+            epoch + 1,
+            n_epoch,
+            train_loss / len(train_loader),
+            train_acc / len(train_loader),
+            train_auc
+        )
+
+        # Validation
         model.train(mode=False)
-        val_loss=0.
-        val_acc=0.
-        output_dict=[]
-        target_dict=[]
+        val_loss = 0.
+        val_acc = 0.
+        output_dict = []
+        target_dict = []
         np.random.seed(seed)
-        for step,data in enumerate(tqdm(val_loader)):
-            img=data['img'].to(device, non_blocking=True).float()
-            target=data['label'].to(device, non_blocking=True).long()
-            
+        for step, data in enumerate(tqdm(val_loader)):
+            img = data['img'].to(device, non_blocking=True).float()
+            target = data['label'].to(device, non_blocking=True).long()
+
             with torch.no_grad():
-                output=model(img)
-                loss=criterion(output,target)
-            
-            loss_value=loss.item()
+                output = model(img)
+                loss = criterion(output, target)
+
+            loss_value = loss.item()
             iter_loss.append(loss_value)
-            val_loss+=loss_value
-            acc=compute_accuray(F.log_softmax(output,dim=1),target)
-            val_acc+=acc
-            output_dict+=output.softmax(1)[:,1].cpu().data.numpy().tolist()
-            target_dict+=target.cpu().data.numpy().tolist()
-        val_losses.append(val_loss/len(val_loader))
-        val_accs.append(val_acc/len(val_loader))
-        val_auc=roc_auc_score(target_dict,output_dict)
-        log_text+="val loss: {:.4f}, val acc: {:.4f}, val auc: {:.4f}".format(
-                        val_loss/len(val_loader),
-                        val_acc/len(val_loader),
-                        val_auc
-                        )
+            val_loss += loss_value
+            acc = compute_accuray(F.log_softmax(output, dim=1), target)
+            val_acc += acc
+            output_dict += output.softmax(1)[:, 1].cpu().data.numpy().tolist()
+            target_dict += target.cpu().data.numpy().tolist()
+
+        val_losses.append(val_loss / len(val_loader))
+        val_accs.append(val_acc / len(val_loader))
+        val_auc = roc_auc_score(target_dict, output_dict)
+
+        log_text += "val loss: {:.4f}, val acc: {:.4f}, val auc: {:.4f}".format(
+            val_loss / len(val_loader),
+            val_acc / len(val_loader),
+            val_auc
+        )
+
         if USE_WANDB:
             wandb.log({
-                'Val/Loss': val_loss/len(val_loader),
-                'Val/Accuracy': val_acc/len(val_loader)
+                'Val/Loss': val_loss / len(val_loader),
+                'Val/Accuracy': val_acc / len(val_loader),
                 'Val/AUC': val_auc,
-                'Train/Loss': train_loss/len(train_loader),
-                'Train/Accuracy': train_acc/len(train_loader),
+                'Train/Loss': train_loss / len(train_loader),
+                'Train/Accuracy': train_acc / len(train_loader),
+                'Train/AUC': train_auc
             })
-     
 
-        if len(weight_dict)<n_weight:
-            save_model_path=os.path.join(save_path+'weights/',"{}_{:.4f}_val.tar".format(epoch+1,val_auc))
-            weight_dict[save_model_path]=val_auc
+        if len(weight_dict) < n_weight:
+            save_model_path = os.path.join(save_path + 'weights/', "{}_{:.4f}_val.tar".format(epoch + 1, val_auc))
+            weight_dict[save_model_path] = val_auc
             torch.save({
-                    "model":model.state_dict(),
-                    "optimizer":model.optimizer.state_dict(),
-                    "epoch":epoch
-                },save_model_path)
-            last_val_auc=min([weight_dict[k] for k in weight_dict])
-
-        elif val_auc>=last_val_auc:
-            save_model_path=os.path.join(save_path+'weights/',"{}_{:.4f}_val.tar".format(epoch+1,val_auc))
+                "model": model.state_dict(),
+                "optimizer": model.optimizer.state_dict(),
+                "epoch": epoch
+            }, save_model_path)
+            last_val_auc = min([weight_dict[k] for k in weight_dict])
+        elif val_auc >= last_val_auc:
+            save_model_path = os.path.join(save_path + 'weights/', "{}_{:.4f}_val.tar".format(epoch + 1, val_auc))
             for k in weight_dict:
-                if weight_dict[k]==last_val_auc:
+                if weight_dict[k] == last_val_auc:
                     del weight_dict[k]
                     os.remove(k)
-                    weight_dict[save_model_path]=val_auc
+                    weight_dict[save_model_path] = val_auc
                     break
             torch.save({
-                    "model":model.state_dict(),
-                    "optimizer":model.optimizer.state_dict(),
-                    "epoch":epoch
-                },save_model_path)
-            last_val_auc=min([weight_dict[k] for k in weight_dict])
-        
+                "model": model.state_dict(),
+                "optimizer": model.optimizer.state_dict(),
+                "epoch": epoch
+            }, save_model_path)
+            last_val_auc = min([weight_dict[k] for k in weight_dict])
+
         logger.info(log_text)
+
         
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
