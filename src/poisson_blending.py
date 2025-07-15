d2cc65dc84d70bdf472adeb775ac0a9675f6ef60
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2025-07-15 15:36:33 +0200

    change base code source

--- src/poisson_blending.py
+++ src/poisson_blending.py
@@ -1,124 +1,108 @@
-"""Poisson image editing.
-https://github.com/PPPW/poisson-image-editing/blob/master/poisson_image_editing.py
+"""
+Poisson Image Editing
+William Emmanuel
+wemmanuel3@gatech.edu
+CS 6745 Final Project Fall 2017
 """
 
 import numpy as np
-import cv2
-import scipy.sparse
-from scipy.sparse.linalg import spsolve
+from scipy.sparse import linalg as linalg
+from scipy.sparse import lil_matrix as lil_matrix
 
-from os import path
+# Helper enum
+OMEGA = 0
+DEL_OMEGA = 1
+OUTSIDE = 2
 
-def laplacian_matrix(n, m):
-    """Generate the Poisson matrix. 
+# Determine if a given index is inside omega, on the boundary (del omega),
+# or outside the omega region
+def point_location(index, mask):
+    if in_omega(index,mask) == False:
+        return OUTSIDE
+    if edge(index,mask) == True:
+        return DEL_OMEGA
+    return OMEGA
 
-    Refer to: 
-    https://en.wikipedia.org/wiki/Discrete_Poisson_equation
+# Determine if a given index is either outside or inside omega
+def in_omega(index, mask):
+    return mask[index] == 1
 
-    Note: it's the transpose of the wiki's matrix 
-    """
-    mat_D = scipy.sparse.lil_matrix((m, m))
-    mat_D.setdiag(-1, -1)
-    mat_D.setdiag(4)
-    mat_D.setdiag(-1, 1)
-        
-    mat_A = scipy.sparse.block_diag([mat_D] * n).tolil()
-    
-    mat_A.setdiag(-1, 1*m)
-    mat_A.setdiag(-1, -1*m)
-    
-    return mat_A
+# Deterimine if a given index is on del omega (boundary)
+def edge(index, mask):
+    if in_omega(index,mask) == False: return False
+    for pt in get_surrounding(index):
+        # If the point is inside omega, and a surrounding point is not,
+        # then we must be on an edge
+        if in_omega(pt,mask) == False: return True
+    return False
 
+# Apply the Laplacian operator at a given index
+def lapl_at_index(source, index):
+    i,j = index
+    val = (4 * source[i,j])    \
+           - (1 * source[i+1, j]) \
+           - (1 * source[i-1, j]) \
+           - (1 * source[i, j+1]) \
+           - (1 * source[i, j-1])
+    return val
 
-def poisson_edit(source, target, mask, offset):
-    """The poisson blending function. 
+# Find the indicies of omega, or where the mask is 1
+def mask_indicies(mask):
+    nonzero = np.nonzero(mask)
+    return zip(nonzero[0], nonzero[1])
 
-    Refer to: 
-    Perez et. al., "Poisson Image Editing", 2003.
-    """
+# Get indicies above, below, to the left and right
+def get_surrounding(index):
+    i,j = index
+    return [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
 
-    # Assume: 
-    # target is not smaller than source.
-    # shape of mask is same as shape of target.
-    y_max, x_max = target.shape[:-1]
-    y_min, x_min = 0, 0
+# Create the A sparse matrix
+def poisson_sparse_matrix(points):
+    # N = number of points in mask
+    N = len(points)
+    A = lil_matrix((N,N))
+    # Set up row for each point in mask
+    for i,index in enumerate(points):
+        # Should have 4's diagonal
+        A[i,i] = 4
+        # Get all surrounding points
+        for x in get_surrounding(index):
+            # If a surrounding point is in the mask, add -1 to index's
+            # row at correct position
+            if x not in points: continue
+            j = points.index(x)
+            A[i,j] = -1
+    return A
 
-    x_range = x_max - x_min
-    y_range = y_max - y_min
-        
-    M = np.float32([[1,0,offset[0]],[0,1,offset[1]]])
-    source = cv2.warpAffine(source,M,(x_range,y_range))
-        
-    mask = mask[y_min:y_max, x_min:x_max]    
-    mask[mask != 0] = 1
-    #mask = cv2.threshold(mask, 127, 1, cv2.THRESH_BINARY)
-    
-    mat_A = laplacian_matrix(y_range, x_range)
+# Main method
+# Does Poisson image editing on one channel given a source, target, and mask
+def process(source, target, mask):
+    indicies = mask_indicies(mask)
+    N = len(indicies)
+    # Create poisson A matrix. Contains mostly 0's, some 4's and -1's
+    A = poisson_sparse_matrix(indicies)
+    # Create B matrix
+    b = np.zeros(N)
+    for i,index in enumerate(indicies):
+        # Start with left hand side of discrete equation
+        b[i] = lapl_at_index(source, index)
+        # If on boundry, add in target intensity
+        # Creates constraint lapl source = target at boundary
+        if point_location(index, mask) == DEL_OMEGA:
+            for pt in get_surrounding(index):
+                if in_omega(pt,mask) == False:
+                    b[i] += target[pt]
 
-    # for \Delta g
-    laplacian = mat_A.tocsc()
+    # Solve for x, unknown intensities
+    x = linalg.cg(A, b)
+    # Copy target photo, make sure as int
+    composite = np.copy(target).astype(int)
+    # Place new intensity on target at given index
+    for i,index in enumerate(indicies):
+        composite[index] = x[0][i]
+    return composite
 
-    # set the region outside the mask to identity    
-    for y in range(1, y_range - 1):
-        for x in range(1, x_range - 1):
-            if mask[y, x] == 0:
-                k = x + y * x_range
-                mat_A[k, k] = 1
-                mat_A[k, k + 1] = 0
-                mat_A[k, k - 1] = 0
-                mat_A[k, k + x_range] = 0
-                mat_A[k, k - x_range] = 0
-
-    # corners
-    # mask[0, 0]
-    # mask[0, y_range-1]
-    # mask[x_range-1, 0]
-    # mask[x_range-1, y_range-1]
-
-    mat_A = mat_A.tocsc()
-
-    mask_flat = mask.flatten()    
-    for channel in range(source.shape[2]):
-        source_flat = source[y_min:y_max, x_min:x_max, channel].flatten()
-        target_flat = target[y_min:y_max, x_min:x_max, channel].flatten()        
-
-        #concat = source_flat*mask_flat + target_flat*(1-mask_flat)
-        
-        # inside the mask:
-        # \Delta f = div v = \Delta g       
-        alpha = 1
-        mat_b = laplacian.dot(source_flat)*alpha
-
-        # outside the mask:
-        # f = t
-        mat_b[mask_flat==0] = target_flat[mask_flat==0]
-        
-        x = spsolve(mat_A, mat_b)
-        #print(x.shape)
-        x = x.reshape((y_range, x_range))
-        #print(x.shape)
-        x[x > 255] = 255
-        x[x < 0] = 0
-        x = x.astype('uint8')
-        #x = cv2.normalize(x, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
-        #print(x.shape)
-
-        target[y_min:y_max, x_min:x_max, channel] = x
-
-    return target
-
-def main():    
-    scr_dir = 'figs/example1'
-    out_dir = scr_dir
-    source = cv2.imread(path.join(scr_dir, "source1.jpg")) 
-    target = cv2.imread(path.join(scr_dir, "target1.jpg"))    
-    mask = cv2.imread(path.join(scr_dir, "mask1.png"), 
-                      cv2.IMREAD_GRAYSCALE) 
-    offset = (0,66)
-    result = poisson_edit(source, target, mask, offset)
-
-    cv2.imwrite(path.join(out_dir, "possion1.png"), result)
-    
-
-if __name__ == '__main__':
-    main()
+# Naive blend, puts the source region directly on the target.
+# Useful for testing
+def preview(source, target, mask):
+    return (target * (1.0 - mask)) + (source * (mask))
