ac0600c9b8562a325f90331b2efda6df892f6a49
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2025-07-21 10:25:20 +0200

    add flat cosine annealing

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -1,7 +1,7 @@
 import torch
 from torch.optim import SGD
 from torch.optim.lr_scheduler import _LRScheduler
-
+import math
 class LinearDecayLR(_LRScheduler):
     def __init__(self, optimizer, n_epoch, start_decay, last_epoch=-1):
         self.start_decay=start_decay
@@ -50,6 +50,35 @@ class LinearDecayLR_LaaNet(_LRScheduler):
         self._last_lr = lr
         print(f'Active Learning Rate --- {lr}')
         return [lr]
+
+    import math
+from torch.optim.lr_scheduler import _LRScheduler
+
+class FlatCosineAnnealingLR(_LRScheduler):
+    def __init__(self, optimizer, total_epochs, flat_epochs, eta_min=0.0, last_epoch=-1):
+        """
+        Args:
+            optimizer (Optimizer): Wrapped optimizer.
+            total_epochs (int): Total number of training epochs.
+            flat_epochs (int): Number of initial epochs with constant LR.
+            eta_min (float): Minimum learning rate.
+            last_epoch (int): The index of last epoch. Default: -1.
+        """
+        self.total_epochs = total_epochs
+        self.flat_epochs = flat_epochs
+        self.eta_min = eta_min
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        if self.last_epoch < self.flat_epochs:
+            return [base_lr for base_lr in self.base_lrs]
+        else:
+            progress = (self.last_epoch - self.flat_epochs) / max(1, self.total_epochs - self.flat_epochs)
+            cosine_decay = 0.5 * (1 + math.cos(math.pi * progress))
+            return [
+                self.eta_min + (base_lr - self.eta_min) * cosine_decay
+                for base_lr in self.base_lrs
+            ]
 
 
 if __name__=='__main__':
